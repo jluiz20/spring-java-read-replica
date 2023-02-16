9645c01998f1b79664670f3f9f5232dd230ba79a
João Luiz Vieira <vieira.jluiz@gmail.com>
2023-02-16 15:03:50 -0800

    started from Spring Initializer

---
+++ src/main/java/xyz/joaovieira/spring/java/readreplica/ReadReplicaApplication.java
@@ -0,0 +1,13 @@
+package xyz.joaovieira.spring.java.readreplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadReplicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadReplicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/joaovieira/spring/java/readreplica/ReadReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.joaovieira.spring.java.readreplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadReplicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
